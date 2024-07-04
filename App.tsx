ebdd8e4cce1afc5d8c635f368997dd9ebafba95f
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-07-04 19:07:18 -0300

    commit 1

--- App.tsx
+++ App.tsx
@@ -1,7 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useState, useEffect } from "react";
+import api from './utils/api';
+
+interface DataTypes {
+  nome: string;
+  descricao: string;
+  preco: number;
+  tipo: string;
+  data_cadastro: Date | string;
+  foto: string;
+}
 
 export default function App() {
+  const [data, setData] = useState<DataTypes[]>([]);
+
+  useEffect(() => {
+    api.get("produtos/")
+      .then((data) => {
+        setData(data.data)
+      })
+      .catch((erro) => {
+        console.error(erro);
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
